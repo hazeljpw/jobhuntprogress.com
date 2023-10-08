cefba0ed676b758bb38d8732a8d50cbfaed24611
Hazel Jeanne Pena <hazeljpw@outlook.com>
2023-10-07 21:44:14 -0400

    Essential folders & app.js initialized

---
+++ app.js
@@ -0,0 +1,11 @@
+// requires
+const express = require('express');
+
+// express app
+const app = express ();
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// listen for requests
+app.listen(3000);
