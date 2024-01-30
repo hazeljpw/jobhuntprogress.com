d372506e37c2ef134b964811f360f56b30ecba6c
Hazel Jeanne Pena <hazeljpw@outlook.com>
2024-01-30 00:58:09 -0500

    Background & Form updates

--- models/progress.js
+++ models/progress.js
@@ -15,6 +15,11 @@ const progressSchema = new Schema({
         type: String,
         required: true,
     },
+    date: {
+        type: Date,
+        timestamps: false,
+        required: true
+    },
     body: {
         type: String,
         required: true
